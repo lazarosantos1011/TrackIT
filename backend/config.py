d89573541feb4269088d22624e706e948d096645
Lázaro Rocha <lazaro.rocha1011@gmail.com>
2025-06-08 20:43:12 -0300

    Atualiza .gitignore para incluir arquivos .env e ajusta configuração do Flask para carregar variáveis de ambiente

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://neondb_owner:npg_U3sW2ezSGAMx@ep-cold-meadow-ac6vtd7m-pooler.sa-east-1.aws.neon.tech/TrackIT_DB?sslmode=require')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
